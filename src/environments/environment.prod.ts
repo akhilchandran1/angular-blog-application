dd8b2ec4367f710a27914b949d65a5105a468912
Akhil Chandran <akhil9216@gmail.com>
2020-07-06 01:02:51 +0100

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+      apiKey: 'AIzaSyCSARYTaLsUTZCrDoUZ6sj7wbBXjVvKWt8',
+        authDomain: 'angular-blog-application.firebaseapp.com',
+        databaseURL: 'https://angular-blog-application.firebaseio.com',
+        projectId: 'angular-blog-application',
+        storageBucket: 'angular-blog-application.appspot.com',
+        messagingSenderId: '360223250608',
+        appId: '1:360223250608:web:320ec979662fec3b6c4375',
+        measurementId: 'G-RTH615WTEX'
+  }
 };
